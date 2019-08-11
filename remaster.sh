36353cf8d60eb607cd92c7b72c927dcbffb1e123
Taqi Raza <mttalpur@gmail.com>
2019-08-11 14:39:59 +0500

    remove logging

--- remaster.sh
+++ remaster.sh
@@ -133,13 +133,9 @@ sudo xorriso -as mkisofs \
        "../extract-cd"
 sudo chown -R $USER ../*iso
 
-ls -al
-
 cd ..
 
 rm original.iso
-
-ls -al
 
 # Write update information for use by AppImageUpdate; https://github.com/AppImage/AppImageSpec/blob/master/draft.md#update-information
 echo "gh-releases-zsync|mmtrt|unity-remix|latest|unity-*amd64.iso.zsync" | dd of="unity-remix-18.04.3-desktop-amd64.iso" bs=1 seek=33651 count=512 conv=notrunc
@@ -147,4 +143,3 @@ echo "gh-releases-zsync|mmtrt|unity-remix|latest|unity-*amd64.iso.zsync" | dd of
 # Write zsync file
 zsyncmake *.iso
 
-ls -al
