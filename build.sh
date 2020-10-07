3e76eb786c260deee536e8415a11fe78fe1f866b
Taqi Raza <mttalpur@gmail.com>
2020-10-07 10:49:51 +0500

    remove spaces

--- build.sh
+++ build.sh
@@ -32,51 +32,52 @@ apt-get install -y -qq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="
   syslinux \
   zsync
 
-  echo >&2 "===]> Info: Build Ubuntu... "
-  /bin/bash -c "
-    ROOT_PATH=${ROOT_PATH} \\
-    WORKING_PATH=${WORKING_PATH} \\
-    CHROOT_PATH=${CHROOT_PATH} \\
-    IMAGE_PATH=${IMAGE_PATH} \\
-    ${ROOT_PATH}/01_build_file_system.sh
-  "
+echo >&2 "===]> Info: Build Ubuntu... "
+ /bin/bash -c "
+   ROOT_PATH=${ROOT_PATH} \\
+   WORKING_PATH=${WORKING_PATH} \\
+   CHROOT_PATH=${CHROOT_PATH} \\
+   IMAGE_PATH=${IMAGE_PATH} \\
+   ${ROOT_PATH}/01_build_file_system.sh
+"
 
-  echo >&2 "===]> Info: Build Image... "
-  /bin/bash -c "
-    ROOT_PATH=${ROOT_PATH} \\
-    WORKING_PATH=${WORKING_PATH} \\
-    CHROOT_PATH=${CHROOT_PATH} \\
-    IMAGE_PATH=${IMAGE_PATH} \\
-    ${ROOT_PATH}/02_build_image.sh
-  "
+echo >&2 "===]> Info: Build Image... "
+ /bin/bash -c "
+   ROOT_PATH=${ROOT_PATH} \\
+   WORKING_PATH=${WORKING_PATH} \\
+   CHROOT_PATH=${CHROOT_PATH} \\
+   IMAGE_PATH=${IMAGE_PATH} \\
+   ${ROOT_PATH}/02_build_image.sh
+"
 
-  echo >&2 "===]> Info: Prepare Boot for ISO... "
-  /bin/bash -c "
-    IMAGE_PATH=${IMAGE_PATH} \\
-    CHROOT_PATH=${CHROOT_PATH} \\
-    ${ROOT_PATH}/03_prepare_iso.sh
-  "
+echo >&2 "===]> Info: Prepare Boot for ISO... "
+ /bin/bash -c "
+   IMAGE_PATH=${IMAGE_PATH} \\
+   CHROOT_PATH=${CHROOT_PATH} \\
+   ${ROOT_PATH}/03_prepare_iso.sh
+"
 
-  echo >&2 "===]> Info: Create ISO... "
-  /bin/bash -c "
-    ROOT_PATH=${ROOT_PATH} \\
-    IMAGE_PATH=${IMAGE_PATH} \\
-    CHROOT_PATH=${CHROOT_PATH} \\
-    ${ROOT_PATH}/04_create_iso.sh
-  "
-  livecd_exitcode=$?
-  if [ "${livecd_exitcode}" -ne 0 ]; then
-    echo "Error building"
-    exit "${livecd_exitcode}"
-  fi
-  ### Zip iso and split it into multiple parts - github max size of release attachment is 2GB, where ISO is sometimes bigger than that
-  cd "${ROOT_PATH}"
+echo >&2 "===]> Info: Create ISO... "
+ /bin/bash -c "
+   ROOT_PATH=${ROOT_PATH} \\
+   IMAGE_PATH=${IMAGE_PATH} \\
+   CHROOT_PATH=${CHROOT_PATH} \\
+   ${ROOT_PATH}/04_create_iso.sh
+"
+livecd_exitcode=$?
+if [ "${livecd_exitcode}" -ne 0 ]; then
+   echo "Error building"
+   exit "${livecd_exitcode}"
+fi
 
-  # Write update information for use by AppImageUpdate; https://github.com/AppImage/AppImageSpec/blob/master/draft.md#update-information
-  echo "gh-releases-zsync|mmtrt|unity-remix|continuous|unity-*20.04*.iso.zsync" | dd of="unity-remix-20.04-desktop-amd64.iso" bs=1 seek=33651 count=512 conv=notrunc
+### Zip iso and split it into multiple parts - github max size of release attachment is 2GB, where ISO is sometimes bigger than that
+cd "${ROOT_PATH}"
 
-  # Write zsync file
-  zsyncmake *.iso
+# Write update information for use by AppImageUpdate; https://github.com/AppImage/AppImageSpec/blob/master/draft.md#update-information
+echo "gh-releases-zsync|mmtrt|unity-remix|continuous|unity-*20.04*.iso.zsync" | dd of="unity-remix-20.04-desktop-amd64.iso" bs=1 seek=33651 count=512 conv=notrunc
+
+# Write zsync file
+zsyncmake *.iso
 
 #   zip -s 1500m "${ROOT_PATH}/output/livecd.zip" "${ROOT_PATH}/unity-remix-20.04-desktop-amd64.iso"
 
